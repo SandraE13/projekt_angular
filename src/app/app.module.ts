fe7d8fb43ddb1bbfe084901b75168f597c5ea404
VORNIA <alex.ev88@gmail.com>
2018-11-15 14:14:07 +0100

    Version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { HeaderUserComponent } from './Header/header-user/header-user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeaderUserComponent
   ],
   imports: [
     BrowserModule,
