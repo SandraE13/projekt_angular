841e835aee7d1f0743340a7b68c7b6d856321efe
sandraei <Sandra.Eissrich@nttdata.com>
2018-11-16 14:14:50 +0100

    https://github.com/SandraE13/projekt_angular.git

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -3,6 +3,7 @@ import { Component, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-side-menu',
+  
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
